9cb91a684f8f1d326e648f4802f7b8d9e86fdd11
bharathkumarna <bn4n5@outlook.com>
2017-02-08 23:19:21 -0600

    test

---
+++ Lab-3/source/Clarifai/src/main/java/ImageAnnotation.java
@@ -0,0 +1,87 @@
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry;
+import okhttp3.OkHttpClient;
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.typography.hershey.HersheyFont;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class ImageAnnotation {
+    public static void main(String[] args) throws IOException {
+        final ClarifaiClient client = new ClarifaiBuilder("CrhSEW7UR2nDOfGk2DpoeSOj1UvmgE0DemVXyoT9", "R2f4_jf9ZJC4LnNT6h2Yyv6Q6jCBbZPJcfHaIxbb")
+                .client(new OkHttpClient()) // OPTIONAL. Allows customization of OkHttp by the user
+                .buildSync(); // or use .build() to get a Future<ClarifaiClient>
+        client.getToken();
+
+        File file = new File("output/mainframes");
+        File[] files = file.listFiles();
+        TreeMap test = new TreeMap(Collections.reverseOrder());
+        TreeMap test1 = new TreeMap(Collections.reverseOrder());
+        for (int i=0; i<files.length;i++){
+            ClarifaiResponse response = client.getDefaultModels().generalModel().predict()
+                    .withInputs(
+                            ClarifaiInput.forImage(ClarifaiImage.of(files[i]))
+                    )
+                    .executeSync();
+            List<ClarifaiOutput<Concept>> predictions = (List<ClarifaiOutput<Concept>>) response.get();
+            MBFImage image = ImageUtilities.readMBF(files[i]);
+            int x = image.getWidth();
+            int y = image.getHeight();
+
+
+
+            List<Concept> data = predictions.get(0).data();
+
+            for (int j = 0; j < 3;j++) {
+                //System.out.println(data.get(j).name() + " - " + data.get(j).value());
+                test.put(data.get(j).name(),data.get(j).value()+i);
+                image.drawText(data.get(j).name(), (int)Math.floor(Math.random()*x), (int) Math.floor(Math.random()*y), HersheyFont.ASTROLOGY, 20, RGBColour.RED);
+            }
+            DisplayUtilities.displayName(image, "image" + i);
+        }
+//Tree Map 1
+        int z=0;
+        Set set = test.entrySet();
+        Iterator count = set.iterator();
+        while(count.hasNext()) {
+            z++;
+            Map.Entry sample = (Map.Entry)count.next();
+            test1.put(sample.getValue(),sample.getKey());
+
+        }
+//Tree Map2
+        System.out.print("From the video, the most identified unique annotations are ");
+        Set set1 = test1.entrySet();
+        Iterator count1 = set1.iterator();
+        while(count1.hasNext()) {
+            Map.Entry sample1 = (Map.Entry) count1.next();
+            System.out.print(","+sample1.getValue());
+
+        }
+        System.out.print(".");
+        System.out.println();
+        System.out.print("The identified words with frame numbers are ");
+        int a[]=new int[z],x=0;
+        String arr[]=new String[z];
+        Set set2 = test1.entrySet();
+        Iterator count2 = set2.iterator();
+        while(count2.hasNext()) {
+            Map.Entry sample1 = (Map.Entry)count2.next();
+            System.out.print(",");
+            System.out.print(sample1.getValue()+" from frame "+((Float)sample1.getKey()).intValue());
+        }
+        System.out.print(".");
+
+    }
+}
